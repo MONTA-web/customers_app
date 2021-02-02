68188199368970f2e29471c5edf70eb01f08d0d9
MONTA-web <montamonta22@icoud.com>
2021-02-02 16:40:24 +0900

    フラッシュメッセージ追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,45 +1,6 @@
 class BlogsController < ApplicationController
   def index
-    
-    @customers = Customer.includes(:user)
-  end
-
-  def new
-    @blog = Blog.new
-  end
-
-  def show
-    @blog = Blog.find(params[:id])
-  end
-
-  def create
-    Blog.create(blog_parameter)
-    redirect_to blogs_path
-  end
-
-  def destroy
-    @blog = Blog.find(params[:id])
-    @blog.destroy
-    redirect_to blogs_path, notice:"削除しました"
-  end
-
-  def edit
-    @blog = Blog.find(params[:id])
-  end
-
-  def update
-    @blog = Blog.find(params[:id])
-    if @blog.update(blog_parameter)
-      redirect_to blogs_path, notice: "編集しました"
-    else
-      render 'edit'
-    end
-  end
-
-  private
-
-  def blog_parameter
-    params.require(:blog).permit(:title, :content, :start_time).merge(user_id: current_user.id)
+   @customers = Customer.includes(:user)
   end
 
 end
